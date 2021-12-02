f9117dd61d1ce8b6fc453d4af90d3b189ce9a736
camillaugolini-iit <camilla.ugolini@iit.it>
2021-12-02 18:26:18 +0100

    commit

--- nanoRMS.sh
+++ nanoRMS.sh
@@ -21,22 +21,22 @@ NANOCOMP_BED="/hpcnfs/scratch/TSSM/cugolini/cov/scripts_new/backupped_data/data_
 
 mkdir -p $WD/PUS7_KD_C37/nanoRMS
 source activate $ENVS/nanoRMS/
-#cp $TRANSCRIPTOME_ASSEMBLY $WD/PUS7_KD_C37/nanoRMS/consensus_extracted.fasta
-#java -jar $NANORMS/epinano_RMS/picard.jar CreateSequenceDictionary REFERENCE=$WD/PUS7_KD_C37/nanoRMS/consensus_extracted.fasta OUTPUT=$WD/PUS7_KD_C37/nanoRMS/consensus_extracted.fasta.dict
-#samtools faidx $WD/PUS7_KD_C37/nanoRMS/consensus_extracted.fasta
-#cd $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOMPORE/map_to_transcriptome/
-#python3 $NANORMS/epinano_RMS/epinano_rms.py -R $WD/PUS7_KD_C37/nanoRMS/consensus_extracted.fasta -b WT_sorted.bam -s $NANORMS/epinano_RMS/sam2tsv.jar -n 4
-#python3 $NANORMS/epinano_RMS/epinano_rms.py -R $WD/PUS7_KD_C37/nanoRMS/consensus_extracted.fasta -b PUS7_KD_sorted.bam -s $NANORMS/epinano_RMS/sam2tsv.jar -n 4
+cp $TRANSCRIPTOME_ASSEMBLY $WD/PUS7_KD_C37/nanoRMS/consensus_extracted.fasta
+java -jar $NANORMS/epinano_RMS/picard.jar CreateSequenceDictionary REFERENCE=$WD/PUS7_KD_C37/nanoRMS/consensus_extracted.fasta OUTPUT=$WD/PUS7_KD_C37/nanoRMS/consensus_extracted.fasta.dict
+samtools faidx $WD/PUS7_KD_C37/nanoRMS/consensus_extracted.fasta
+cd $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOMPORE/map_to_transcriptome/
+python3 $NANORMS/epinano_RMS/epinano_rms.py -R $WD/PUS7_KD_C37/nanoRMS/consensus_extracted.fasta -b WT_sorted.bam -s $NANORMS/epinano_RMS/sam2tsv.jar -n 4
+python3 $NANORMS/epinano_RMS/epinano_rms.py -R $WD/PUS7_KD_C37/nanoRMS/consensus_extracted.fasta -b PUS7_KD_sorted.bam -s $NANORMS/epinano_RMS/sam2tsv.jar -n 4
 #conda deactivate
 #source activate $ENVS/R_env/
 #cd $NANORMS/predict_rna_mod
 #Rscript --vanilla $NANORMS/predict_rna_mod/Pseudou_prediction_pairedcondition_transcript.R -f $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOMPORE/map_to_transcriptome/WT_sorted.per.site.baseFreq.csv -s $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOMPORE/map_to_transcriptome/PUS7_KD_sorted.per.site.baseFreq.csv 
 
 #### calculate stoichiometry
-cd $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOMPORE/map_to_transcriptome/
+#cd $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOMPORE/map_to_transcriptome/
 ## Convert BAM to TSV
 #samtools view -h WT_sorted.bam | java -jar $NANORMS/epinano_RMS/sam2tsv.jar -r $WD/PUS7_KD_C37/nanoRMS/consensus_extracted.fasta  > WT.bam.tsv 
 #samtools view -h PUS7_KD_sorted.bam | java -jar $NANORMS/epinano_RMS/sam2tsv.jar -r $WD/PUS7_KD_C37/nanoRMS/consensus_extracted.fasta  > PUS7_KD.bam.tsv
 ## Run EpiNano
-python3.6 TSV_to_Variants_Freq.py3 -f WT.bam.tsv -t 10 
+#python3.6 TSV_to_Variants_Freq.py3 -f WT.bam.tsv -t 10 
 
