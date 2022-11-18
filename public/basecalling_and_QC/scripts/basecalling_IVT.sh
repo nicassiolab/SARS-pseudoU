e0a9cb9b07a09cd9ea003e928148b4cbe40d2f27
camillaugolini-iit <camilla.ugolini@iit.it>
2022-11-18 17:31:11 +0100

    rebasecalling of IVT

--- public/basecalling_and_QC/scripts/basecalling_IVT.sh
+++ public/basecalling_and_QC/scripts/basecalling_IVT.sh
@@ -9,7 +9,7 @@ MOUNT_DIR="/hpcnfs/scratch/"
 IMG="/hpcnfs/scratch/TSSM/cugolini/cov/img"
 SINGC="singularity run -B $MOUNT_DIR $IMG/guppy_latest.sif"
 WD="/hpcnfs/scratch/temporary/camilla_FN/basecalling/cov"
-THREADS=10
+THREADS=12
 HAC_FILE="/hpcnfs/scratch/TSSM/cugolini/tools/ont-guppy-cpu/data/rna_r9.4.1_70bps_hac.cfg"
 MIN_Q_SCORE=7
 KIT="SQK-RNA002"
@@ -19,6 +19,6 @@ FLOWCELL="FLO-MIN106"
 
 mkdir -p $WD/IVT/fast5
 cp $IVT/*.fast5 $WD/IVT/fast5
-$SINGC guppy_basecaller -i $WD/IVT/fast5 -s $WD --num_callers $THREADS -c $HAC_FILE --recursive --min_qscore $MIN_Q_SCORE --disable_pings --reverse_sequence true --u_substitution true --trim_strategy rna
+$SINGC guppy_basecaller -i $WD/IVT/fast5 -s $WD --cpu_threads_per_caller 4 --num_callers $THREADS -c $HAC_FILE --recursive --min_qscore $MIN_Q_SCORE --disable_pings --reverse_sequence true --u_substitution true --trim_strategy rna
 
 
