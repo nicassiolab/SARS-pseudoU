e69fe36cbb898692a25ed681fd028694ce35dde5
camillaugolini-iit <camilla.ugolini@iit.it>
2022-12-14 11:12:13 +0100

    commit

--- PUS7KD_C37.sh
+++ PUS7KD_C37.sh
@@ -40,15 +40,15 @@ NANOCOMP_BED="/hpcnfs/scratch/TSSM/cugolini/cov/scripts_new/backupped_data/data_
 ############################################################################ map WT and KD to  VIRAL TRANSCRIPTOME (NRCEQ ASSEMBLY-EXTRACTION) ##########################################################################
 
 # map to the assembly(EXTRACTION)
-mkdir -p $WD/PUS7_KD_C37/map_to_recap_assembly
+#mkdir -p $WD/PUS7_KD_C37/map_to_recap_assembly
 #singularity exec -B /hpcnfs/scratch/ $singpore minimap2 -t 20 -ax map-ont -p 0 -N 10 $TRANSCRIPTOME_ASSEMBLY $DATA/fastq_PUS7_C37/PUS7_KD.fastq > /hpcnfs/scratch/temporary/camilla_TL/temp_dir_nanop/PUS7_KD.sam
 #singularity exec -B /hpcnfs/scratch/ $singpore minimap2 -t 20 -ax map-ont -p 0 -N 10 $TRANSCRIPTOME_ASSEMBLY $DATA/fastq_PUS7_C37/WT.fastq > $WD/PUS7_KD_C37/map_to_recap_assembly/WT.sam
 
 ###     CREATE ALIGNMENTS BACKUP, NANOCOUNT AND NANOCOMPORE FILE PREP
 
-mkdir -p $WD/PUS7_KD_C37/map_to_recap_assembly/ALIGNMENTS_BACKUP
-mkdir -p $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOUNT/counts
-mkdir -p $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOMPORE/map_to_transcriptome
+#mkdir -p $WD/PUS7_KD_C37/map_to_recap_assembly/ALIGNMENTS_BACKUP
+#mkdir -p $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOUNT/counts
+#mkdir -p $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOMPORE/map_to_transcriptome
 
 #for filename in $WD/PUS7_KD_C37/map_to_recap_assembly/*.sam; do
 #        name=${filename##*/}
@@ -73,19 +73,19 @@ mkdir -p $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOMPORE/map_to_transcriptome
 #/hpcnfs/scratch/TSSM/cugolini/tools/f5c/f5c-v0.6/f5c_x86_64_linux index -d $PUS7_KD/fast5_pass $DATA/fastq_PUS7_C37/PUS7_KD.fastq
 #/hpcnfs/scratch/TSSM/cugolini/tools/f5c/f5c-v0.6/f5c_x86_64_linux index -d $WT/fast5_pass $DATA/fastq_PUS7_C37/WT.fastq
 
-mkdir -p $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOMPORE/eventalign
+#mkdir -p $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOMPORE/eventalign
 #/hpcnfs/scratch/TSSM/cugolini/tools/f5c/f5c-v0.6/f5c_x86_64_linux eventalign --rna --min-mapq 0 -t 20 -r $DATA/fastq_PUS7_C37/PUS7_KD.fastq -b $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOMPORE/map_to_transcriptome/PUS7_KD_sorted.bam --g $TRANSCRIPTOME_ASSEMBLY --samples --print-read-names --scale-events | singularity exec -B /hpcnfs/scratch/ /hpcnfs/scratch/TSSM/cugolini/cov/img/nanocompore_8d6a70c.img  NanopolishComp Eventalign_collapse -o $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOMPORE/eventalign/PUS7_KD/
 
 #/hpcnfs/scratch/TSSM/cugolini/tools/f5c/f5c-v0.6/f5c_x86_64_linux eventalign --rna --min-mapq 0 -t 20 -r $DATA/fastq_PUS7_C37/WT.fastq -b $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOMPORE/map_to_transcriptome/WT_sorted.bam --g $TRANSCRIPTOME_ASSEMBLY --samples --print-read-names --scale-events | singularity exec -B /hpcnfs/scratch/ /hpcnfs/scratch/TSSM/cugolini/cov/img/nanocompore_8d6a70c.img  NanopolishComp Eventalign_collapse -o $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOMPORE/eventalign/WT
 
 #singularity pull docker://tleonardi/nanocompore:v1.0.4
 #mv nanocompore_v1.0.4.sif /hpcnfs/scratch/TSSM/cugolini/cov/img/
-nanocomp104="/hpcnfs/scratch/TSSM/cugolini/cov/img/nanocompore_v1.0.4.sif"
+#nanocomp104="/hpcnfs/scratch/TSSM/cugolini/cov/img/nanocompore_v1.0.4.sif"
 
 #singularity exec -B /hpcnfs/scratch/ $nanocomp104 nanocompore sampcomp --file_list1 $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOMPORE/eventalign/WT/out_eventalign_collapse.tsv --file_list2 $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOMPORE/eventalign/PUS7_KD/out_eventalign_collapse.tsv  --label1 WT --label2 PUS7_KD --fasta $NANOCOMP_FA --outpath $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOMPORE/sampcomp/PUS7_KD_WT/ --overwrite --downsample_high_coverage 5000 --allow_warnings --pvalue_thr 0.01 --min_coverage 30 --logit --nthreads 3 --bed $NANOCOMP_BED
 
-mkdir -p $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOMPORE/sampcomp/PUS7_KD_IVT/
-mkdir -p $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOMPORE/sampcomp/WT_IVT/
+#mkdir -p $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOMPORE/sampcomp/PUS7_KD_IVT/
+#mkdir -p $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOMPORE/sampcomp/WT_IVT/
 
 #for filename in /hpcnfs/scratch/TSSM/cugolini/cov/analysis/7_samples_extraction/nanocompore/HUXELERATE_RESULTS/extraction/eventalign/IVT/eventalign*; do
 #        base=${filename##*/eventalign_}
@@ -102,7 +102,7 @@ mkdir -p $WD/PUS7_KD_C37/map_to_recap_assembly/NANOCOMPORE/sampcomp/WT_IVT/
 
 ############################################################################ map WT and KD to  VIRAL GENOME ##########################################################################
 
-mkdir -p $WD/PUS7_KD_C37/map_to_genome
+#mkdir -p $WD/PUS7_KD_C37/map_to_genome
 #singularity exec -B /hpcnfs/scratch/ $singpore minimap2 -k 8 -w 1 -ax splice -g 30000 -G 30000 -A1 -B2 -O2,24 -E1,0 -C0 -z 400,200 --no-end-flt -F 40000 -N 32 --splice-flank=no --max-chain-skip=40 -un -p 0.7 -t 8 $GENOME_FA $DATA/fastq_PUS7_C37/WT.fastq > $WD/PUS7_KD_C37/map_to_genome/WT.sam
 #singularity exec -B /hpcnfs/scratch/ $singpore samtools view -h -F 2324 -Sb $WD/PUS7_KD_C37/map_to_genome/WT.sam > $WD/PUS7_KD_C37/map_to_genome/WT.bam
 #singularity exec -B /hpcnfs/scratch/ $singpore samtools sort $WD/PUS7_KD_C37/map_to_genome/WT.bam > $WD/PUS7_KD_C37/map_to_genome/WT_sorted.bam
@@ -118,11 +118,11 @@ mkdir -p $WD/PUS7_KD_C37/map_to_genome
 
 #singularity exec -B /hpcnfs/scratch/ $nanocomp104 nanocompore sampcomp --file_list1 $WD/PUS7_KD_C37/map_to_genome/NANOCOMPORE/eventalign/WT/out_eventalign_collapse.tsv  --file_list2 $WD/PUS7_KD_C37/map_to_genome/NANOCOMPORE/eventalign/PUS7_KD/out_eventalign_collapse.tsv   --label1 WT --label2 PUS7_KD --fasta $GENOME_FA --outpath $WD/PUS7_KD_C37/map_to_genome/NANOCOMPORE/sampcomp/PUS7_KD_WT/ --overwrite --downsample_high_coverage 5000 --allow_warnings --pvalue_thr 0.01 --min_coverage 30 --logit --nthreads 24
 
-IVT="/hpcnfs/scratch/TSSM/tleonardi/SARS-CoV-2-datasets/Kim_2020/fastq/IVT/ont_research.fa"
+#IVT="/hpcnfs/scratch/TSSM/tleonardi/SARS-CoV-2-datasets/Kim_2020/fastq/IVT/ont_research.fa"
 
-WD="/hpcnfs/scratch/temporary/camilla_TL/genome_analysis"
-mkdir -p $WD/PUS7_KD_C37/map_to_genome/NANOCOMPORE/eventalign
-#singularity exec -B /hpcnfs/scratch/ $singpore minimap2 -k 8 -w 1 -ax splice -g 30000 -G 30000 -A1 -B2 -O2,24 -E1,0 -C0 -z 400,200 --no-end-flt -F 40000 -N 32 --splice-flank=no --max-chain-skip=40 -un -p 0.7 -t 12 $GENOME_FA $IVT > $WD/PUS7_KD_C37/map_to_genome/IVT.sam
+#WD="/hpcnfs/scratch/temporary/camilla_TL/genome_analysis"
+#mkdir -p $WD/PUS7_KD_C37/map_to_genome/NANOCOMPORE/eventalign
+##singularity exec -B /hpcnfs/scratch/ $singpore minimap2 -k 8 -w 1 -ax splice -g 30000 -G 30000 -A1 -B2 -O2,24 -E1,0 -C0 -z 400,200 --no-end-flt -F 40000 -N 32 --splice-flank=no --max-chain-skip=40 -un -p 0.7 -t 12 $GENOME_FA $IVT > $WD/PUS7_KD_C37/map_to_genome/IVT.sam
 #singularity exec -B /hpcnfs/scratch/ $singpore samtools view -h -F 2324 -Sb $WD/PUS7_KD_C37/map_to_genome/IVT.sam > $WD/PUS7_KD_C37/map_to_genome/IVT.bam
 #singularity exec -B /hpcnfs/scratch/ $singpore samtools sort $WD/PUS7_KD_C37/map_to_genome/IVT.bam > $WD/PUS7_KD_C37/map_to_genome/IVT_sorted.bam
 #singularity exec -B /hpcnfs/scratch/ $singpore samtools index $WD/PUS7_KD_C37/map_to_genome/IVT_sorted.bam
@@ -133,7 +133,7 @@ mkdir -p $WD/PUS7_KD_C37/map_to_genome/NANOCOMPORE/eventalign
 #awk '$2<=120' $WD/PUS7_KD_C37/map_to_genome/IVT_sorted.bed | cut -f4 > $WD/PUS7_KD_C37/map_to_genome/IVT_sorted_0_120.txt
 #python /hpcnfs/scratch/TSSM/cugolini/cov/scripts_new/recappable/orf_analysis/extract_reads.py -b $WD/PUS7_KD_C37/map_to_genome/IVT_sorted.bam -n $WD/PUS7_KD_C37/map_to_genome/IVT_sorted_0_120.txt -o $WD/PUS7_KD_C37/map_to_genome/IVT_sorted_0_120.bam
 #singularity exec -B /hpcnfs/scratch/ $singpore samtools index $WD/PUS7_KD_C37/map_to_genome/IVT_sorted_0_120.bam
-/hpcnfs/scratch/TSSM/cugolini/tools/f5c/f5c-v0.6/f5c_x86_64_linux eventalign --rna --min-mapq 0 -t 12 -r $IVT -b $WD/PUS7_KD_C37/map_to_genome/IVT_sorted_0_120.bam --g $GENOME_FA --samples --print-read-names --scale-events | singularity exec -B /hpcnfs/scratch/ /hpcnfs/scratch/TSSM/cugolini/cov/img/nanocompore_8d6a70c.img  NanopolishComp Eventalign_collapse -o $WD/PUS7_KD_C37/map_to_genome/NANOCOMPORE/eventalign/IVT
+#/hpcnfs/scratch/TSSM/cugolini/tools/f5c/f5c-v0.6/f5c_x86_64_linux eventalign --rna --min-mapq 0 -t 12 -r $IVT -b $WD/PUS7_KD_C37/map_to_genome/IVT_sorted_0_120.bam --g $GENOME_FA --samples --print-read-names --scale-events | singularity exec -B /hpcnfs/scratch/ /hpcnfs/scratch/TSSM/cugolini/cov/img/nanocompore_8d6a70c.img  NanopolishComp Eventalign_collapse -o $WD/PUS7_KD_C37/map_to_genome/NANOCOMPORE/eventalign/IVT
 
 #singularity exec -B /hpcnfs/scratch/ $nanocomp104 nanocompore sampcomp --file_list1 $WD/PUS7_KD_C37/map_to_genome/NANOCOMPORE/eventalign/WT/out_eventalign_collapse.tsv --file_list2 $WD/PUS7_KD_C37/map_to_genome/NANOCOMPORE/eventalign/IVT/out_eventalign_collapse.tsv  --label1 WT --label2 IVT --fasta $GENOME_FA --outpath $WD/PUS7_KD_C37/map_to_genome/NANOCOMPORE/sampcomp/WT_IVT/ --overwrite --downsample_high_coverage 5000 --allow_warnings --pvalue_thr 0.01 --min_coverage 30 --logit --nthreads 3
 
@@ -144,9 +144,9 @@ mkdir -p $WD/PUS7_KD_C37/map_to_genome/NANOCOMPORE/eventalign
 
 ############################################################################ map WT and KD to HUMAN TRANSCRIPTOME  ##########################################################################
 
-mkdir -p $WD/PUS7_KD_C37/map_to_human_transcriptome
-mkdir -p $WD/PUS7_KD_C37/map_to_human_transcriptome/ALIGNMENTS_BACKUP/
-mkdir -p $WD/PUS7_KD_C37/map_to_human_transcriptome/NANOCOUNT/counts
+#mkdir -p $WD/PUS7_KD_C37/map_to_human_transcriptome
+#mkdir -p $WD/PUS7_KD_C37/map_to_human_transcriptome/ALIGNMENTS_BACKUP/
+#mkdir -p $WD/PUS7_KD_C37/map_to_human_transcriptome/NANOCOUNT/counts
 
 #singularity exec -B /hpcnfs/scratch/ $singpore minimap2 -ax map-ont -uf -p 0 -N 10 -t 20 -I100g $REF_DATA/transcriptome_fasta_primary_assembly.fa $DATA/fastq_PUS7_C37/WT.fastq > $WD/PUS7_KD_C37/map_to_human_transcriptome/WT.sam
 #singularity exec -B /hpcnfs/scratch/ $singpore minimap2 -ax map-ont -uf -p 0 -N 10 -t 20 -I100g $REF_DATA/transcriptome_fasta_primary_assembly.fa $DATA/fastq_PUS7_C37/PUS7_KD.fastq > $WD/PUS7_KD_C37/map_to_human_transcriptome/PUS7_KD.sam
@@ -182,3 +182,7 @@ mkdir -p  $WD/PUS7_KD_C37/map_to_human_transcriptome/NANOCOMPORE/eventalign/
 ###	 identify miscall in UGUAR fragments 
 #singularity exec -B /hpcnfs/scratch/ /hpcnfs/scratch/FN/TL/cugolini/cov/analysis/PUS7_KD_C37/map_to_genome/pileup/bcftools_v1.9-1-deb_cv1.sif bcftools mpileup -a FORMAT/AD,INFO/AD -f $GENOME_FA -R $WD/PUS7_KD_C37/map_to_genome/pileup/regions_UGUAR_fragments.bed $WD/PUS7_KD_C37/map_to_genome/WT_sorted.bam > $WD/PUS7_KD_C37/map_to_genome/pileup/counts_WT.bcf
 #singularity exec -B /hpcnfs/scratch/ /hpcnfs/scratch/FN/TL/cugolini/cov/analysis/PUS7_KD_C37/map_to_genome/pileup/bcftools_v1.9-1-deb_cv1.sif bcftools mpileup -a FORMAT/AD,INFO/AD -f $GENOME_FA -l $WD/PUS7_KD_C37/map_to_genome/pileup/regions_UGUAR_fragments.bed $WD/PUS7_KD_C37/map_to_genome/PUS7_KD_sorted.bam > $WD/PUS7_KD_C37/map_to_genome/pileup/counts_PUS7_KD.bcf
+
+
+
+
