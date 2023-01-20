a0a04cee5bf690ea2366a5e618f5e4a9b5ffed5e
camillaugolini-iit <camilla.ugolini@iit.it>
2023-01-20 11:46:11 +0100

    changed main directory

--- scripts/general/config.sh
+++ scripts/general/config.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 BASEDIR="/hpcnfs/scratch/TSSM/cugolini/cov"
-FILES="/hpcnfs/scratch/FN/TL/cugolini/cov/scripts/files" 	                                # directory of configuration files used for the analysis
+FILES="/hpcnfs/scratch/FN/TL/cugolini/cov/SARS-CoV-2_pseudoU/scripts/files" 	                # directory of configuration files used for the analysis
 IMG="$BASEDIR/img"
 MOUNT_DIR="/hpcnfs/scratch"                                     	                        # insert directory to mount on singularity images (needs to contain data and working directory)
 GENOME_FA="$FILES/edited.fa"                                             	                # viral genome fasta
+GENOME_FAI="$FILES/edited.fa.fai"								# viral genome fasta index
 TRANSCRIPTOME_ASSEMBLY="$FILES/consensus_extracted.fa"						# viral transcriptome NRCeq assembly from https://doi.org/10.1093/nar/gkac144
 TRANSCRIPTOME_ASSEMBLY_FAI="$FILES/consensus_extracted.fa.fai"
 NANOCOMP_BED="$FILES/aln_consensus_name_commas.bed"						# NRCeq assembly bedfile used for nanocompore processing
