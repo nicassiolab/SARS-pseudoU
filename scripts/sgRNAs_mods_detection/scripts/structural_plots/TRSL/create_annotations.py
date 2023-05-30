5646e1941b7a137d55c747fd414a8b49aa629c13
camillaugolini-iit <camilla.ugolini@iit.it>
2023-05-30 14:40:03 +0200

    structural representation of TRS-L

---
+++ scripts/sgRNAs_mods_detection/scripts/structural_plots/TRSL/create_annotations.py
@@ -0,0 +1,107 @@
+#!/bin/python
+import numpy as np
+from collections import *
+import matplotlib as mpl
+from matplotlib import cm
+import matplotlib.pyplot as plt
+from matplotlib import colors
+from string import ascii_letters
+import sys
+
+nanocomp_res=sys.argv[1]
+nanocomp_bed=sys.argv[2]
+sto=sys.argv[3]
+tx=sys.argv[4]
+thr=float(sys.argv[5])
+pval_col=int(sys.argv[6])
+real_start=int(sys.argv[7])
+rfam_id=sys.argv[8]
+scale_outfile=sys.argv[9]
+lor_thr=0.5
+lor_col=12
+
+nanocomp_start=0
+symbol="M"
+
+with open(nanocomp_bed) as f:
+    for line in f:
+        line=line.split('\t')
+        if(line[3] == tx): nanocomp_start=int(line[1])
+
+offset=nanocomp_start-real_start+2
+#print(f"offset: {offset}")
+
+pos=dict()
+with open(nanocomp_res) as f:
+    f.readline()
+    for line in f:
+        line=line.split('\t')
+        if(line[3]== tx ):
+            if(float(line[pval_col])<=thr and abs(float(line[lor_col]))>=lor_thr):
+                pos[int(line[0])+offset] = float(line[pval_col])
+
+#print(f"positions to annotate: {pos}")
+
+
+all=defaultdict(list)
+for kmer in pos.keys():
+    for p in range(kmer,kmer+5):
+        all[p].append(pos[kmer])
+
+min_p={k:-np.log10(min(v)) for k,v in all.items()}
+
+# Make color map
+cmap = cm.get_cmap('YlOrRd', len(ascii_letters)) 
+max_min_p = max(min_p.values())
+if max_min_p>10: max_min_p=10
+norm=colors.Normalize(vmin=min(min_p.values()), vmax=max_min_p, clip=True) 
+cols=dict()
+for i in range(len(ascii_letters)):
+    cols[ascii_letters[i]] = cmap(i)
+
+
+fig, ax = plt.subplots(figsize=(6, 1))
+fig.subplots_adjust(bottom=0.5)
+cb1 = mpl.colorbar.ColorbarBase(ax, cmap=cmap, norm=norm, orientation='horizontal', extend="max")
+cb1.set_label('Some Units')
+fig.savefig(scale_outfile)
+#print(cols)
+# Center kmers
+pos = [k for i in pos.keys() for k in range(i,i+5)]
+
+used_key=list()
+with open(sto) as f:
+    for line in f:
+        line=line.split()
+        if(len(line)>1):
+            if(line[0]==rfam_id):
+                seq=line[1]
+                counter=1
+                annot=""
+                annot_p=""
+                for i in seq:
+                    if(i!="-"):
+                        if counter in pos:
+                            annot+=symbol
+                            c = cmap(norm(min_p[counter]))
+                            for k,v in cols.items():
+                                if v==c:
+                                    annot_p+=k
+                                    used_key.append(k)
+                        else:
+                            annot+="."
+                            annot_p+="."
+                        counter+=1
+                    else:
+                        annot+="."
+                        annot_p+="."
+print("#=GC R2R_XLABEL_m            ", annot)
+print("#=GC R2R_XLABEL_p            ", annot_p)
+
+for k,v in cols.items():
+    if k in used_key:
+        r=int(v[0]*255)
+        g=int(v[1]*255)
+        b=int(v[2]*255)
+        print(f"#=GF R2R_oneseq {rfam_id} shade_along_backbone p:{k} rgb:{r},{g},{b}")
+
---
+++ scripts/sgRNAs_mods_detection/scripts/structural_plots/TRSL/generate_figures.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+# load variables from general configuration file
+if [ -z "$path" ]
+then
+      CURR_DIR=$1
+else
+      CURR_DIR=$path
+fi
+
+CONFIG=$(echo $CURR_DIR | rev | cut -d'/' -f7- |rev)                                    # obtain configuration file directory
+source $CONFIG/scripts/scripts/general/config.sh
+
+
+WD="$BASEDIR/analysis/sgRNAs_mods_detection/guppy_initial/structural_plots/TRSL"
+ENVS="/hpcnfs/home/ieo5215/miniconda/envs"
+template="$CURR_DIR/TRSL.sto"
+tx="5820681b-6191-45dd-959c-96a8097aafdd|2255::NC_045512v2:14-29871"
+nanocomp_res="$BASEDIR/analysis/sgRNAs_mods_detection/guppy_initial/CaCo2/nanocompore/sampcomp/5820681b-6191-45dd-959c-96a8097aafdd_2255_NC_045512v2_14-29871/outnanocompore_results.tsv"
+real_start=0
+rfam_id="TRSL"
+
+source activate $ENVS/r2r/
+r2r ${WD}/TRSL.meta ${WD}/TRSL.pdf
+cat <(head -n -1 $template) <(python ${SCRIPTDIR2}/create_annotations.py $nanocomp_res $NANOCOMP_BED $template $tx 0.01 6 $real_start $rfam_id ${WD}/image_file.svg) <(echo //) > ${WD}/TRSL_annotated.sto
+r2r ${WD}/TRSL.meta ${WD}/TRSL.pdf
